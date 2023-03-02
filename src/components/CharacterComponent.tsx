62d13034555932c72b310a5f230fcfad8c26641e
xTye <iwatchtyler@gmail.com>
2023-03-01 19:30:17 -0500

    Styling fixes

--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -24,7 +24,7 @@ const CharacterComponent: Component<{
           <div>{props.class}</div>
         </div>
         <div class="text-base text-yellow">{props.title}</div>
-        <div class="text-sm h-52">{props.description}</div>
+        <div class="text-sm h-52" innerHTML={props.description} />
         <div class="flex justify-between gap-2 items-center">
           <div class="text-sm">{props.mainAttack}</div>
           <img class="w-1/5" src={props.mainAttackImage} alt="main attack" />
