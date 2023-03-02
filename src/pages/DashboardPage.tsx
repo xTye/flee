fecfabea537ca5b99727b66a0df27036290ce5ee
xTye <iwatchtyler@gmail.com>
2023-03-01 19:28:07 -0500

    Re add dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,7 +2,6 @@ import { Component, onMount, createMemo, onCleanup } from "solid-js";
 import { useNavigate } from "@solidjs/router";
 import { navbarHeight } from "../components/navbar/NavbarComponent";
 import { useSession } from "../auth";
-import { useAdobe } from "../hooks/AdobeHooks";
 
 const DashboardPage: Component = () => {
   const [session, actions] = useSession();
@@ -20,10 +19,6 @@ const DashboardPage: Component = () => {
     docsDiv.style.height = window.innerHeight - navbarHeight.height + "px";
   });
 
-  onMount(() => {
-    useAdobe("/characters/instance.pdf", "instance.pdf");
-  });
-
   return (
     <>
       <div
@@ -31,20 +26,15 @@ const DashboardPage: Component = () => {
         class="flex flex-col gap-6 min-h-screen bg-background text-text py-12"
       >
         <div class="text-text text-4xl px-40">Player Dashboard</div>
-        <div class="flex justify-between">
-          {/* <embed
-            src="https://ddb.ac/characters/25454097/3ZUMUO"
-            class="h-screen w-4/12 h-[600px]"
-          ></embed> */}
-          <div id="adobe-dc-view" class="h-screen h-[600px]"></div>
-          {/* <embed
+        <div class="flex justify-between gap-2">
+          <embed
             src="https://docs.google.com/document/d/1IlCK-w6unVGRH0dPMZ97MyuksUQsAazfPhOwgFjsX50/edit?embedded=true"
-            class="h-screen w-1/2 h-[600px]"
+            class="h-screen basis-1/2 h-[600px]"
           ></embed>
           <embed
             src="https://docs.google.com/spreadsheets/d/1sZkFUeiAcg6ZmkJ1JfVq21pKyilsHa3yI2M0BeTauRo/edit#gid=0?embedded=true"
-            class="h-screen w-1/2 h-[600px]"
-          ></embed> */}
+            class="h-screen basis-1/2 h-[600px]"
+          ></embed>
         </div>
       </div>
     </>
