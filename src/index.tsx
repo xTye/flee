81dda98cb5f296be8b8c5df2a7d1d2e11e70444f
xTye <iwatchtyler@gmail.com>
2025-02-16 12:58:47 -0500

    Fix auth

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import { connectFunctionsEmulator, getFunctions } from "firebase/functions"
 
 const firebaseConfig = {
   apiKey: "AIzaSyASrzGbTJs9bJwzcLasfpOW1nbq7ymfsyE",
-  authDomain: "fleednd.com",
+  authDomain: "flee-website.firebaseapp.com",
   projectId: "flee-website",
   storageBucket: "flee-website.appspot.com",
   messagingSenderId: "796176359929",
